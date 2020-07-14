74a677cca5820893a003d550355665c64cd08938
Felipe Erick dos Santos - FSO <felipe.santos@iteris.local>
2020-07-14 14:06:28 -0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,11 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="grey"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          :src="require('./assets/logo.png')"
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          width="100"
-        />
+  <div id="material-kit">
+    <div :class="{ 'nav-open': NavbarStore.showNavbar }">
+      <router-view name="header" />
+      <div>
+        <router-view />
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://vitaminanerd.com.br"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Acessar</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+      <router-view name="footer" />
+    </div>
+  </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
-</script>
